a270a1410d5dea57e05ef931c6f7709adaac9644
Remy <remy.e.francis@gmail.com>
2018-04-13 16:19:12 -0400

    change

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="index.html">Home</a>
+                            <a class="nav-link" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="portfolio.html">Portfolio</a>
