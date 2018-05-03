8e483e76e8bf0aa6627b36d407f51c54aee447ef
Remy <remy.e.francis@gmail.com>
2018-05-03 16:25:16 -0400

    pricing pages

--- mail.php
+++ mail.php
@@ -35,7 +35,7 @@ if ($message === ''){
 }
 
 $content="From: $name \n Email: $email \n Phone: $phone \n Company: $company \n Message: $message";
-$recipient = "";
+$recipient = "developer@remyfrancis.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
